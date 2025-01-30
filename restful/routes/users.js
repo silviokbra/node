496c2135462944e356427b2063e75ed13d936052
Silvio Seferin <silvioseferin@gmail.com>
2025-01-30 02:33:02 -0300

    Adicionando trecho para o express validator

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -30,6 +30,8 @@ module.exports = (app) => {
 
     route.post((req, res) => {
 
+        if (!app.utils.validator.user(app, req, res)) return false;
+
         db.insert(req.body, (err, user) => {
 
             if (err) {
@@ -60,6 +62,8 @@ module.exports = (app) => {
     });
 
     routeId.put((req, res) => {
+
+        if (!app.utils.validator.user(app, req, res)) return false;
 
         db.update({ _id: req.params.id }, req.body, err => {
 
--- restful/utils/error.js
+++ restful/utils/error.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    send:(err, req, res, code = 400)=>{
+    send: (err, req, res, code = 400) => {
 
         console.log(`error: ${err}`);
         res.status(code).json({
