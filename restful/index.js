11389a9cdeb111f24f108217238f0d58034b30fb
Silvio Seferin <silvioseferin@gmail.com>
2025-01-30 00:54:51 -0300

    Iniciando server com express

--- restful/index.js
+++ restful/index.js
@@ -1,40 +1,33 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
+app.get('/', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Tipe', 'text/html');
+    res.end('<h1>Olá</h1>');
     console.log('URL:', req.url);
     console.log('METHOD:', req.method);
 
-    switch (req.url) {
+});
 
-        case '/':
+app.get('/users', (req, res) => {
 
-            res.statusCode = 200;
-            res.setHeader('Content-Tipe', 'text/html');
-            res.end('<h1>Olá</h1>');
+    res.statusCode = 200;
+    res.setHeader('Content-Tipe', 'application/json');
+    res.json({
+        users: [{
+            name: 'Hcode',
+            email: 'contato@hcode.com.br',
+            id: 1
+        }]
 
-            break;
-
-        case '/users':
-
-            res.statusCode = 200;
-            res.setHeader('Content-Tipe', 'application/json');
-            res.end(JSON.stringify({
-                users: [{
-                    name: 'Hcode',
-                    email: 'contato@hcode.com.br',
-                    id: 1
-                }]
-
-            }));
-
-            break;
-
-    }
+    });
 
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
 
     console.log('servidor rodando!');
 
